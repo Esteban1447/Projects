c8033c45f7501a25c70e04475818cc05624b6236
Esteban1447 <esteban.robledo.rc@gmail.com>
2026-06-30 20:29:44 -0500

    Codigo comentado

--- Proyecto.2/Pagina_Piloto_List/script.js
+++ Proyecto.2/Pagina_Piloto_List/script.js
@@ -6,18 +6,25 @@ const completedTasks = document.getElementById('completedTasks');
 
 let tasks = [];
 
+// Esta funcion es la que se encarga de todo el html que se crea dentro de la pagina, los task
+
 function renderTask(){
     taskList.innerHTML = '';
 
+    // Comprueba si el arreglo esta vacio
     if (tasks.length === 0){
+
         const empty = document.createElement('p');
         empty.className = 'empty-state';
         empty.textContent = 'No hay tareas. Añade una tarea para empezar.'
-        taskList.appendChild(empty);
+        taskList.appendChild(empty);    
     } else {
         tasks.forEach((task) => {
             const item = document.createElement('li');
+
+        // Le pone la clase task-item completed si la tarea esta compleada (cuestion de estilo tachado que tiene al completada)
             item.className = `task-item${task.completed ? ' completed' : ''}`;
+            // condicion ? si verdadero : si falso
 
             const label = document.createElement('label');
             const checkbox = document.createElement('input')
@@ -28,16 +35,21 @@ function renderTask(){
             const text = document.createElement('span');
             text.textContent = task.text;
 
+            // Esto añade checkbox y text adentro de la etiqueta label creada
             label.appendChild(checkbox);
             label.appendChild(text);
-
+        
             const actions = document.createElement('div');
             actions.className = 'task-actions'
+
+            // El boton de borrar manda como parametro el id de la task
 
             const deleteButton = document.createElement('button');
             deleteButton.textContent = 'Eliminar';
             deleteButton.type = 'button'
             deleteButton.addEventListener('click', () => removeTask(task.id));
+
+            // Aqui se empaceta deletebutton en actions, actions en item junt con label (que previamente tiene otros hijos) y luego item se concentra en el contenedor principal que es taskList
 
             actions.appendChild(deleteButton);
             item.appendChild(label);
@@ -46,6 +58,7 @@ function renderTask(){
         });
     
     }
+    // Esto simplemente refresca los datos de el total de tareas y el total de tareas completadas cada que se ejecuta renderTask
     updateSummary();
 }
 
@@ -54,6 +67,8 @@ function updateSummary() {
     const completedCount = tasks.filter((task) => task.completed).length;
     completedTasks.textContent = `Completadas: ${completedCount}`;
 }
+
+// Trim hace que se eliminen los espacios, asi que si alguien quiere enviar algo con caracteres vacios la tarea simplenente no se crea
 
 function addTask(text) {
     if(!text.trim()) return;
@@ -67,16 +82,25 @@ function addTask(text) {
     renderTask();
 }
 
+// toggleTask espera como parametro el id para ver que tarea queremos cambiarle el estado de completed
+// La comprovacion es si el id de la task es exactamente igual cambia el estado de task a true, si no rendereiza la task comun y corriente
+
 function toggleTask(id) {
     tasks = tasks.map((task) => task.id === id ? {...task, completed: !task.completed} : task
 );
 renderTask();
 }
 
+// Se borra con un filter, anteriormente le pedimos de parametro id al boton de borrar, esto lo que hace es comprobar si el id que se mando es el mismo id de el elemento se esta verificando, si no lo es, lo deja en la lista, si si lo es no lo incluye en la lista, por lo tanto se borra
+
+// Resumido, crea una nueva lista sin el elemento que se excluyo
+
 function removeTask(id) {
     tasks = tasks.filter((task) => task.id!== id);
     renderTask();
 }
+
+
 
 taskForm.addEventListener('submit', (event) => {
     event.preventDefault();
